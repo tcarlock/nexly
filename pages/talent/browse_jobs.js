c8148140f7145ebf80f263327ff189dfccfad77c
Tim Carlock <tcarlock@Tims-MacBook-Pro.local>
2022-06-08 00:16:02 -0400

    Initial commit

---
+++ pages/talent/browse_jobs.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
+
+export default function JobSearch() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        
+      </main>
+    </div>
+  )
+}
